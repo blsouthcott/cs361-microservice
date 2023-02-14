8a17e5e6d76a1a0e9793579b290b9aea1d1f9a0b
Ben <benjamin.southcott@gmail.com>
2023-02-13 21:10:25 -0700

    add better error messaging

--- app.py
+++ app.py
@@ -21,14 +21,22 @@ def total_time():
     print(body)
     if not (intersections := body.get("intersections")):
         return "intersections is a required field", 404
+    if type(intersections) is not list:
+        return "intersections field must be an array", 404
     for intersection in intersections:
         if not (ramps := intersection.get("ramps")):
             return "each intersection must include a ramps field", 404
+        if type(ramps) is not list:
+            return "ramps field must be an array", 404
         for ramp in ramps:
-            if not (style := ramp.get("style")) or not (inspection_type := ramp.get("inspectionType")):
+            if (not (style := ramp.get("style"))) or (not (inspection_type := ramp.get("inspectionType"))):
                 return "ramp field must have style and inspectionType fields", 404
             style = style.lower()
             inspection_type = inspection_type.lower()
+            if not times.get(style):
+                return "Invalid ramp style. Please see README for list of valid options", 404
+            if inspection_type != "full" and not times.get(inspection_type):
+                return "inspectionType can only be either FULL or GO_BACK"
             if inspection_type == "go_back":
                 time += times[inspection_type]
             else:
